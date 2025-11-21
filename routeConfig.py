4fe18e4d17dce5316aea40a79d77aad0448defc4
Richard <richardruizs1998@gmail.com>
2025-11-21 16:19:30 -0600

    Ajuste de rutas para lograr subir el server con python3 server.py en raiz

--- routeConfig.py
+++ routeConfig.py
@@ -1,9 +1,21 @@
+from routes.auth_routes import auth_bp
 from routes.user_routes import user_bp
+from flask import render_template
 
 def register_routes(app):
     print("[✔] Registrando rutas...")
 
-    # Registrar rutas para usuarios
+    # Registrar API
+    app.register_blueprint(auth_bp)
     app.register_blueprint(user_bp)
+
+    # Registrar rutas para header y footer
+    @app.route('/header.html')
+    def header_partial():
+        return render_template('header.html')
+
+    @app.route('/footer.html')
+    def footer_partial():
+        return render_template('footer.html')
 
     print("[✔] Rutas registradas exitosamente")
--- static/js/header.js
+++ static/js/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("header.html")
+  fetch("/header.html")
     .then((response) => {
       if (!response.ok) throw new Error("Error al cargar el header");
       return response.text();
@@ -11,13 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
       const main = document.querySelector("main");
 
       if (header && main) {
-        function ajustarMain() {
-          main.style.paddingTop = header.offsetHeight + "px";
-        }
+        const ajustarMain = () => main.style.paddingTop = header.offsetHeight + "px";
 
         ajustarMain();
         window.addEventListener("resize", ajustarMain);
-
         window.addEventListener("scroll", ajustarMain);
       }
     })
